5a59655808bb7f7282dfd74bb1a159a6f364c009
tanzeem131 <dev.tanzeem@gmail.com>
2024-10-07 18:29:54 +0530

    Added new problem

---
+++ All-important-DSA-questions/Power-of-an-element.js
@@ -0,0 +1,40 @@
+//(number)**(odd)
+//(number)**(even)
+//(number)**(positive)
+//(number)**(negative)
+
+function findPowerOfAnElement(a, n) {
+  if (n == 0) {
+    return 1;
+  } else if (n == 1) {
+    return a;
+  } else {
+    let mid;
+    if (n < 0) {
+      mid = Math.floor(-n / 2);
+    } else {
+      mid = Math.floor(n / 2);
+    }
+    const b = findPowerOfAnElement(a, mid);
+    const result = b * b;
+    if (n > 0) {
+      if (n % 2 == 0) {
+        return result;
+      } else {
+        return result * a;
+      }
+    } else {
+      if (n % 2 == 0) {
+        return 1 / result;
+      } else {
+        return 1 / (result * a);
+      }
+    }
+  }
+}
+
+//Driver code
+const a = -3;
+const n = 3;
+const result = findPowerOfAnElement(a, n);
+console.log(result);
