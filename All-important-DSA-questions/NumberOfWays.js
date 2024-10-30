5db1febea94419359386321164d1934cb783100e
tanzeem131 <dev.tanzeem@gmail.com>
2024-10-30 23:34:58 +0530

    Solved new Problem

--- All-important-DSA-questions/NumberOfWays.js
+++ All-important-DSA-questions/NumberOfWays.js
@@ -1,3 +1,6 @@
+//You are climbing a staircase. It takes n steps to reach the top.
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
 function findTotalWays(n) {
   if (n == 1 || n == 2) {
     return n;
