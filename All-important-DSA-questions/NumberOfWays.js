c18084c3df37af0f04789ea8ac991f5f54755788
tanzeem131 <dev.tanzeem@gmail.com>
2024-10-30 23:34:06 +0530

    Solved new Problem

--- All-important-DSA-questions/NumberOfWays.js
+++ All-important-DSA-questions/NumberOfWays.js
@@ -2,11 +2,12 @@ function findTotalWays(n) {
   if (n == 1 || n == 2) {
     return n;
   } else {
+    //recursion
     return findTotalWays(n - 1) + findTotalWays(n - 2);
   }
 }
 
 //Driver code
-const n = 4;
+const n = 4; //but for larger value of n recursion will fail-->"OVERLAPPING SUBPROBLEM"(findTotalWays(n - 2) is overlapping in next iteration and so on..)--->>then we will use Dynamic programming
 const result = findTotalWays(n);
 console.log(result);
